5e9ad46aa6f5ac590cae865bcd81a328f0ea5d2d
1610ayush <67241639+1610ayush@users.noreply.github.com>
2020-10-16 17:23:34 +0530

    Add files via upload

---
+++ 1-D_array.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+#height=[1.73,1.68,1.71,1.89,1.79]
+#
+#weight=[65.4,59.2,63.6,88.4,68.7]
+#
+#np_height=np.array(height)
+#np_weight=np.array(weight)
+#bmi=np_weight/ np_height ** 2
+#print(bmi)
+
+#python_list=[1,2,3]
+#numpy_array=np.array(python_list)
+#
+#x=python_list+python_list
+#print(x)
+#
+#y=numpy_array+numpy_array
+#print(y)
+
+#print(bmi[1])
+
+#print(bmi[bmi>23])
+
+#print(type(np_weight))
+
+#z=np.array([True,1,2])+np.array([3,4,False])
+#True will get converted to 1 and False to 0
+#print(z)
